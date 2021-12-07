e5c8e6946de1c3f22a774e72226f470df6fffe47
DYLAN VILLARREAL TOSCANO <54459436+dylanson25@users.noreply.github.com>
2021-12-07 11:06:00 -0600

    Avance

---
+++ inmobiliaria/src/components/HomeS.js
@@ -0,0 +1,15 @@
+import React, {useEffect, useState} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+
+
+const HomeS = () => {
+ 
+  return (
+    <>
+      <Text>Hola Homeless</Text>
+    </>
+  );
+};
+
+export default HomeS;
+
